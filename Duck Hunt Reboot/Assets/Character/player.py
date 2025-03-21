6f25a4fc205cdd96f9faad53a776e38196c2cdb4
MikaM-droid <mikamonsma@gmail.com>
2025-03-21 13:14:18 +0100

    update

--- Duck Hunt Reboot/Assets/Character/player.py
+++ Duck Hunt Reboot/Assets/Character/player.py
@@ -2,22 +2,36 @@ import pygame
 
 class Player():
     def __init__(self, x, y):
-        self.image = pygame.image.load("Duck Hunt Reboot/Assets/Character/cat.png")  # Load player sprite
-        self.image = pygame.transform.scale(self.image, (50, 50))  # Resize
+        self.image = pygame.image.load("Duck Hunt Reboot/Assets/Character/cat.png")
+        self.image = pygame.transform.scale(self.image, (50, 50)) 
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
-        self.speed = 5  # Movement speed
+        
+        self.speed = 5  #Horizontal movement speed
+        self.jump_power = -12  #Jump velocity
+        self.gravity = 0.5  #Gravity strength
+        self.velocity_y = 0  #Vertical movement speed
+        self.on_ground = True  #Check if the player is on the ground
 
     def move(self, keys):
-        if keys[pygame.K_LEFT]:
+        if keys[pygame.K_a]:
             self.rect.x -= self.speed
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_d]:
             self.rect.x += self.speed
-        if keys[pygame.K_UP]:
-            self.rect.y -= self.speed
-        if keys[pygame.K_DOWN]:
-            self.rect.y += self.speed
+
+        if keys[pygame.K_w] and self.on_ground:
+            self.velocity_y = self.jump_power
+            self.on_ground = False  # Player is in the air
+
+        #Gravity applying
+        self.velocity_y += self.gravity
+        self.rect.y += self.velocity_y
+
+        #Collision with the ground (temporary ground level at y=500)
+        if self.rect.y >= 500:
+            self.rect.y = 500
+            self.velocity_y = 0
+            self.on_ground = True  # Reset jump when touching the ground
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
-
